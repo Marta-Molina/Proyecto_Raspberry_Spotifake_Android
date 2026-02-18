3692a6870dec60376b9d40816f4910c787fd7975
Marta-Molina <martamolinaescalona@hotmail.com>
2026-02-18 21:40:24 +0100

    añadida busqueda y  likes

--- app/src/main/java/com/example/appmusica/domain/repository/CancionRepository.kt
+++ app/src/main/java/com/example/appmusica/domain/repository/CancionRepository.kt
@@ -3,7 +3,7 @@ package com.example.appmusica.domain.repository
 import com.example.appmusica.domain.model.Cancion
 
 interface CancionRepository {
-    suspend fun getCanciones(): List<Cancion>
+    suspend fun getCanciones(nombre: String? = null, artista: String? = null, album: String? = null): List<Cancion>
     suspend fun getCancion(id: Int): Cancion?
     suspend fun addCancion(cancion: Cancion)
     suspend fun updateCancion(id: Int, cancion: Cancion)

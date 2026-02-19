567da86916e64927b2a9a0663a9d70b7a300e5be
Marta-Molina <martamolinaescalona@hotmail.com>
2026-02-19 09:46:09 +0100

    añadiendo imports de Fragment

--- app/src/main/java/com/example/appmusica/presentation/canciones/CancionesFragment.kt
+++ app/src/main/java/com/example/appmusica/presentation/canciones/CancionesFragment.kt
@@ -3,6 +3,7 @@ package com.example.appmusica.presentation.canciones
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
--- app/src/main/java/com/example/appmusica/presentation/playlists/PlaylistSongsFragment.kt
+++ app/src/main/java/com/example/appmusica/presentation/playlists/PlaylistSongsFragment.kt
@@ -3,6 +3,7 @@ package com.example.appmusica.presentation.playlists
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController

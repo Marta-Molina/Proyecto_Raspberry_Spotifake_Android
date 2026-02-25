1aaaa2fee2541d04d153a126a67d108594032e3a
Marta-Molina <martamolinaescalona@hotmail.com>
2026-02-25 14:23:06 +0100

    serializando urls

--- app/src/main/java/com/example/appmusica/domain/model/Album.kt
+++ app/src/main/java/com/example/appmusica/domain/model/Album.kt
@@ -1,9 +1,13 @@
 package com.example.appmusica.domain.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Album(
     val id: Int,
     val nombre: String,
     val artista: String? = null,
     val artistaId: Int,
+
+    @SerializedName("portadaUrl")
     val portadaUrl: String?
 )
--- app/src/main/java/com/example/appmusica/domain/model/Artista.kt
+++ app/src/main/java/com/example/appmusica/domain/model/Artista.kt
@@ -1,7 +1,11 @@
 package com.example.appmusica.domain.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Artista(
     val id: Int,
     val nombre: String,
+
+    @SerializedName("fotoUrl")
     val fotoUrl: String?
 )
